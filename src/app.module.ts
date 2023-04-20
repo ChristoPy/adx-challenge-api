7672460926c3fce8ad9112d57d8fd0ef63c79336
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-04-20 17:55:41 -0300

    Load variables from environment.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,7 +11,10 @@ import { jwtConstants } from './auth/constants';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
